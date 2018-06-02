eb7b109cd04421538429764a4bf0d56888d803fb
pamarthid <pamarthidurgarao1@gmail.com>
2018-06-02 13:22:26 +0530

    ui build changes

--- src/main/java/com/pamarthi/generator/api_generator/config/SwaggerConfig.java
+++ src/main/java/com/pamarthi/generator/api_generator/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.pamarthi.generator.api_generator.config;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,22 +17,35 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ {
+public class SwaggerConfig /* extends WebMvcConfigurationSupport */ {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.pamarthi.generator.api_generator")).paths(PathSelectors.any()).build()
-				.apiInfo(metaData());
+				.apis(RequestHandlerSelectors.basePackage("com.pamarthi.generator.api_generator"))
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
 	}
 
 	private ApiInfo metaData() {
 		return new ApiInfoBuilder().title("API Generator").version("1.0.0").build();
 	}
 
-	/*@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+	@Bean
+	public EmbeddedServletContainerCustomizer singlePageAppRouting() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+			}
+		};
+	}
 
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}*/
+	/*
+	 * @Override protected void addResourceHandlers(ResourceHandlerRegistry
+	 * registry) {
+	 * registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+	 * "classpath:/META-INF/resources/");
+	 * 
+	 * registry.addResourceHandler("/webjars/**").addResourceLocations(
+	 * "classpath:/META-INF/resources/webjars/"); }
+	 */
 }
